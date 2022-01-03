58b924ea2e314dc670b7cfce025ac3b82dcc8f61
Archibald-prog <archpod@gmail.com>
2022-01-03 06:03:52 +0300

    Сделал альтернативный вариант решения ДЗ к Уроку 3

--- lesson_3/server.py
+++ lesson_3/server.py
@@ -22,11 +22,16 @@ def handle_message(message):
 
 def main():
     global CONFIGS
+    # вызываем функцию load_configs() из модуля utils.py
+    # с параметром по умолчанию is_server=True
     CONFIGS = load_configs()
     try:
+        # если при запуске скрипта передается флаг -p, то после него
+        # в списке параметров sys.argv доолжен быть указан порт для прослушивания
         if '-p' in sys.argv:
             listen_port = int(sys.argv[sys.argv.index('-p') + 1])
         else:
+            # если флага -р нет - слушаем порт по умолчанию
             listen_port = CONFIGS.get('DEFAULT_PORT')
         if not 65535 >= listen_port >= 1024:
             raise ValueError
@@ -39,8 +44,11 @@ def main():
         sys.exit(1)
 
     try:
+        # если при запуске скрипта передается флаг -а, то после него
+        # в списке параметров sys.argv доолжен быть указан IP-адрес для прослушивания
         if '-a' in sys.argv:
             listen_address = sys.argv[sys.argv.index('-a') + 1]
+            # если флага -а нет - слушаем все доступные адреса
         else:
             listen_address = ''
 
@@ -48,15 +56,18 @@ def main():
         print(
             'После \'a\'- необходимо указать адрес для ')
         sys.exit(1)
-
+    # создаем сокет TCP
     transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # приявязыаем сокет к IP-адресу и порту клиента
     transport.bind((listen_address, listen_port))
-
+    # максимальное число одновременных запросов берем из config.json
     transport.listen(CONFIGS.get('MAX_CONNECTIONS'))
 
     while True:
         client, client_address = transport.accept()
         try:
+            # в функцию get_message() передаем конфигурации,
+            # которые возвращает функция load_configs() из модуля utils.py
             message = get_message(client, CONFIGS)
             response = handle_message(message)
             send_message(client, response, CONFIGS)
